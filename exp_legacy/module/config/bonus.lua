e9f82db1548c256c86c3bda9a7e036c7bce75cb9
PHIDIAS <phidias@aperx.org>
2024-12-29 14:10:55 +0900

    .

--- exp_legacy/module/config/bonus.lua
+++ exp_legacy/module/config/bonus.lua
@@ -18,16 +18,16 @@ return {
     ]]
     pts = {
         rate = {
-            ['Senior Administrator'] = 1.5,
-            ['Administrator'] = 1.4,
-            ['Senior Moderator'] = 1.3,
-            ['Moderator'] = 1.2,
-            ['Trainee Moderator'] = 1.2,
-            ['Board Member'] = 1.1,
-            ['Supporter'] = 1.1,
-            ['Partner'] = 1.1,
-            ['Senior Member'] = 1.1,
-            ['Member'] = 1
+            ["Senior Administrator"] = 1.5,
+            ["Administrator"] = 1.4,
+            ["Senior Moderator"] = 1.3,
+            ["Moderator"] = 1.2,
+            ["Trainee Moderator"] = 1.2,
+            ["Board Member"] = 1.1,
+            ["Supporter"] = 1.1,
+            ["Partner"] = 1.1,
+            ["Senior Member"] = 1.1,
+            ["Member"] = 1
         },
         base = 172
     },
@@ -38,19 +38,19 @@ return {
         count = 50
     },
     conversion = {
-        ['cmms'] = 'character_mining_speed_modifier',
-        ['crs'] = 'character_running_speed_modifier',
-        ['ccs'] = 'character_crafting_speed_modifier',
-        ['cisb'] = 'character_inventory_slots_bonus',
-        ['chb'] = 'character_health_bonus',
-        ['crdb'] = 'character_reach_distance_bonus',
+        ["cmms"] = "character_mining_speed_modifier",
+        ["crs"] = "character_running_speed_modifier",
+        ["ccs"] = "character_crafting_speed_modifier",
+        ["cisb"] = "character_inventory_slots_bonus",
+        ["chb"] = "character_health_bonus",
+        ["crdb"] = "character_reach_distance_bonus",
         --[[
-        ['cpdb'] = 'character_item_pickup_distance_bonus'
+        ["cpdb"] = "character_item_pickup_distance_bonus"
         ]]
     },
     player_special_bonus_rate = 300,
     player_special_bonus = {
-        ['personal_battery_recharge'] = {
+        ["personal_battery_recharge"] = {
             -- 1 MW
             value = 4,
             max = 8,
@@ -61,7 +61,7 @@ return {
         }
     },
     player_bonus = {
-        ['character_mining_speed_modifier'] = {
+        ["character_mining_speed_modifier"] = {
             value = 2,
             max = 4,
             scale = 0.5,
@@ -69,7 +69,7 @@ return {
             cost = 10,
             is_percentage = true
         },
-        ['character_running_speed_modifier'] = {
+        ["character_running_speed_modifier"] = {
             value = 1,
             max = 2,
             scale = 0.25,
@@ -77,7 +77,7 @@ return {
             cost = 60,
             is_percentage = true
         },
-        ['character_crafting_speed_modifier'] = {
+        ["character_crafting_speed_modifier"] = {
             value = 6,
             max = 12,
             scale = 0.5,
@@ -85,7 +85,7 @@ return {
             cost = 4,
             is_percentage = true
         },
-        ['character_inventory_slots_bonus'] = {
+        ["character_inventory_slots_bonus"] = {
             value = 60,
             max = 120,
             scale = 5,
@@ -93,7 +93,7 @@ return {
             cost = 2,
             is_percentage = false
         },
-        ['character_health_bonus'] = {
+        ["character_health_bonus"] = {
             value = 100,
             max = 200,
             scale = 25,
@@ -101,7 +101,7 @@ return {
             cost = 2,
             is_percentage = false
         },
-        ['character_reach_distance_bonus'] = {
+        ["character_reach_distance_bonus"] = {
             value = 8,
             max = 16,
             scale = 2,
@@ -109,12 +109,12 @@ return {
             cost = 1,
             is_percentage = false,
             combined_bonus = {
-                'character_resource_reach_distance_bonus',
-                'character_build_distance_bonus'
+                "character_resource_reach_distance_bonus",
+                "character_build_distance_bonus"
             }
         },
         --[[
-        ['character_item_pickup_distance_bonus'] = {
+        ["character_item_pickup_distance_bonus"] = {
             value = 0,
             max = 20,
             scale = 1,
@@ -122,7 +122,7 @@ return {
             cost = 1,
             is_percentage = false
         },
-        ['character_loot_pickup_distance_bonus'] = {
+        ["character_loot_pickup_distance_bonus"] = {
             value = 0,
             max = 20,
             scale = 1,
@@ -130,7 +130,7 @@ return {
             cost = 1,
             is_percentage = false
         },
-        ['character_item_drop_distance_bonus'] = {
+        ["character_item_drop_distance_bonus"] = {
             value = 0,
             max = 20,
             scale = 1,
@@ -142,7 +142,7 @@ return {
     },
     force_bonus = {
         --[[
-        ['character_mining_speed_modifier'] = {
+        ["character_mining_speed_modifier"] = {
             value = 0,
             max = 6,
             scale = 0.5,
@@ -150,7 +150,7 @@ return {
             cost = 10,
             is_percentage = true
         },
-        ['character_running_speed_modifier'] = {
+        ["character_running_speed_modifier"] = {
             value = 0,
             max = 3,
             scale = 0.25,
@@ -158,7 +158,7 @@ return {
             cost = 40,
             is_percentage = true
         },
-        ['character_crafting_speed_modifier'] = {
+        ["character_crafting_speed_modifier"] = {
             value = 0,
             max = 16,
             scale = 1,
@@ -166,7 +166,7 @@ return {
             cost = 4,
             is_percentage = true
         },
-        ['character_inventory_slots_bonus'] = {
+        ["character_inventory_slots_bonus"] = {
             value = 0,
             max = 200,
             scale = 10,
@@ -174,14 +174,14 @@ return {
             cost = 2,
             is_percentage = false
         },
-        ['character_health_bonus'] = {
+        ["character_health_bonus"] = {
             value = 0,
             max = 400,
             scale = 50,
             cost = 4,
             is_percentage = false
         },
-        ['character_reach_distance_bonus'] = {
+        ["character_reach_distance_bonus"] = {
             value = 0,
             max = 24,
             scale = 2,
@@ -189,11 +189,11 @@ return {
             cost = 1,
             is_percentage = false,
             combined_bonus = {
-                'character_resource_reach_distance_bonus',
-                'character_build_distance_bonus'
+                "character_resource_reach_distance_bonus",
+                "character_build_distance_bonus"
             }
         },
-        ['worker_robots_speed_modifier'] = {
+        ["worker_robots_speed_modifier"] = {
             value = 0,
             max = 0,
             scale = 0,
@@ -202,7 +202,7 @@ return {
             is_percentage = false
         },
         ]]
-        ['worker_robots_battery_modifier'] = {
+        ["worker_robots_battery_modifier"] = {
             value = 1,
             max = 1,
             scale = 1,
@@ -210,7 +210,7 @@ return {
             cost = 1,
             is_percentage = false
         },
-        ['worker_robots_storage_bonus'] = {
+        ["worker_robots_storage_bonus"] = {
             value = 1,
             max = 1,
             scale = 1,
@@ -218,7 +218,7 @@ return {
             cost = 1,
             is_percentage = false
         },
-        ['following_robots_lifetime_modifier'] = {
+        ["following_robots_lifetime_modifier"] = {
             value = 1,
             max = 1,
             scale = 1,
@@ -227,7 +227,7 @@ return {
             is_percentage = false
         },
         --[[
-        ['character_item_pickup_distance_bonus'] = {
+        ["character_item_pickup_distance_bonus"] = {
             value = 0,
             max = 20,
             scale = 1,
@@ -235,7 +235,7 @@ return {
             cost = 1,
             is_percentage = false
         },
-        ['character_loot_pickup_distance_bonus'] = {
+        ["character_loot_pickup_distance_bonus"] = {
             value = 0,
             max = 20,
             scale = 1,
@@ -243,7 +243,7 @@ return {
             cost = 1,
             is_percentage = false
         },
-        ['character_item_drop_distance_bonus'] = {
+        ["character_item_drop_distance_bonus"] = {
             value = 0,
             max = 20,
             scale = 1,
@@ -251,7 +251,7 @@ return {
             cost = 1,
             is_percentage = false
         },
-        ['character_trash_slot_count'] = {
+        ["character_trash_slot_count"] = {
             value = 0,
             max = 0,
             scale = 0,
@@ -259,7 +259,7 @@ return {
             cost = 1,
             is_percentage = false
         },
-        ['mining_drill_productivity_bonus'] = {
+        ["mining_drill_productivity_bonus"] = {
             value = 0,
             max = 0,
             scale = 0,
@@ -267,7 +267,7 @@ return {
             cost = 1,
             is_percentage = false
         },
-        ['train_braking_force_bonus'] = {
+        ["train_braking_force_bonus"] = {
             value = 0,
             max = 0,
             scale = 0,
@@ -275,7 +275,7 @@ return {
             cost = 1,
             is_percentage = false
         },
-        ['laboratory_speed_modifier'] = {
+        ["laboratory_speed_modifier"] = {
             value = 0,
             max = 0,
             scale = 0,
@@ -283,7 +283,7 @@ return {
             cost = 1,
             is_percentage = false
         },
-        ['laboratory_productivity_bonus'] = {
+        ["laboratory_productivity_bonus"] = {
             value = 0,
             max = 0,
             scale = 0,
@@ -291,7 +291,7 @@ return {
             cost = 1,
             is_percentage = false
         },
-        ['inserter_stack_size_bonus'] = {
+        ["inserter_stack_size_bonus"] = {
             value = 0,
             max = 0,
             scale = 0,
@@ -299,7 +299,7 @@ return {
             cost = 1,
             is_percentage = false
         },
-        ['stack_inserter_capacity_bonus'] = {
+        ["stack_inserter_capacity_bonus"] = {
             value = 0,
             max = 0,
             scale = 0,
@@ -307,7 +307,7 @@ return {
             cost = 1,
             is_percentage = false
         },
-        ['artillery_range_modifier'] = {
+        ["artillery_range_modifier"] = {
             value = 0,
             max = 0,
             scale = 0,
@@ -319,7 +319,7 @@ return {
     },
     surface_bonus = {
         --[[
-        ['solar_power_multiplier'] = {
+        ["solar_power_multiplier"] = {
             value = 1,
             max = 1000,
             scale = 1,
