b32e854398462c772e9b5a6c93941b4b04671aa7
PHIDIAS <phidias@aperx.org>
2024-12-29 14:12:50 +0900

    .

--- exp_legacy/module/config/advanced_start.lua
+++ exp_legacy/module/config/advanced_start.lua
@@ -4,7 +4,7 @@
 --- These are called factories because they return another function
 -- use these as a simple methods of adding new items
 -- they will do most of the work for you
--- ['item-name'] = factory(params)
+-- ["item-name"] = factory(params)
 -- luacheck:ignore 212/amount_made 212/items_made 212/player
 
 -- Use these to adjust for ticks ie game.tick < 5*minutes
@@ -12,7 +12,7 @@
 local seconds, minutes, hours = 60, 3600, 216000
 
 --- Use to make a split point for the number of items given based on time
--- ['stone-furnace']=cutoff_time(5*minutes, 4,0) -- before 5 minutes give four items after 5 minutes give none
+-- ["stone-furnace"]=cutoff_time(5*minutes, 4,0) -- before 5 minutes give four items after 5 minutes give none
 local function cutoff_time(time, before, after)
     return function(amount_made, items_made, player)
         if game.tick < time then
@@ -24,7 +24,7 @@ local function cutoff_time(time, before, after)
 end
 
 --- Use to make a split point for the number of items given based on amount made
--- ['firearm-magazine']=cutoff_amount_made(100, 10, 0) -- give 10 items until 100 items have been made
+-- ["firearm-magazine"]=cutoff_amount_made(100, 10, 0) -- give 10 items until 100 items have been made
 --[[
 local function cutoff_amount_made(amount, before, after)
     return function(amount_made, items_made, player)
@@ -38,7 +38,7 @@ end
 ]]
 
 --- Same as above but will not give any items if x amount has been made of another item, useful for tiers
--- ['light-armor']=cutoff_amount_made_unless(5, 0,1,'heavy-armor',5) -- give light armor once 5 have been made unless 5 heavy armor has been made
+-- ["light-armor"]=cutoff_amount_made_unless(5, 0,1,"heavy-armor",5) -- give light armor once 5 have been made unless 5 heavy armor has been made
 --[[
 local function cutoff_amount_made_unless(amount, before, after, second_item, second_amount)
     return function(amount_made, items_made, player)
@@ -56,7 +56,7 @@ end
 ]]
 
 -- Use for mass production items where you want the amount to change based on the amount already made
--- ['iron-plate']=scale_amount_made(5*minutes, 10, 10) -- for first 5 minutes give 10 items then after apply a factor of 10
+-- ["iron-plate"]=scale_amount_made(5*minutes, 10, 10) -- for first 5 minutes give 10 items then after apply a factor of 10
 --[[
 local function scale_amount_made(amount, before, scalar)
     return function(amount_made, items_made, player)
@@ -73,7 +73,7 @@ end
     Common values
     game.tick is the amount of time the game has been on for
     amount_made is the amount of that item which has been made
-    items_made('item-name') will return the amount of any item made
+    items_made("item-name") will return the amount of any item made
     player is the player who just spawned
     hours, minutes, seconds are the number of ticks in each unit of time
 ]]
@@ -85,28 +85,28 @@ return {
     enemy_expansion = false, --- @setting enemy_expansion a catch all for in case the map settings file fails to load
     chart_radius = 10 * 32, --- @setting chart_radius the number of tiles that will be charted when the map starts
     items = { --- @setting items items and there condition for being given
-        ['iron-ore']=cutoff_time(20 * minutes, 80, 0),
-        ['stone']=cutoff_time(20 * minutes, 20, 0),
-        ['coal']=cutoff_time(20 * minutes, 20, 0),
-        -- ['burner-mining-drill']=cutoff_time(20 * minutes, 8, 0),
-        -- ['stone-furnace']=cutoff_time(20 * minutes, 8, 0),
-        ['submachine-gun'] = 1,
-        ['piercing-rounds-magazine'] = 20,
-        ['construction-robot'] = 10,
-        ['modular-armor'] = 1,
-        ['solar-panel-equipment'] = 16,
-        ['belt-immunity-equipment'] = 1,
-        ['battery-equipment'] = 2,
-        ['personal-roboport-equipment'] = 1
+        ["iron-ore"] = cutoff_time(20 * minutes, 80, 0),
+        ["stone"] = cutoff_time(20 * minutes, 20, 0),
+        ["coal"] = cutoff_time(20 * minutes, 20, 0),
+        -- ["burner-mining-drill"]=cutoff_time(20 * minutes, 8, 0),
+        -- ["stone-furnace"]=cutoff_time(20 * minutes, 8, 0),
+        ["submachine-gun"] = 1,
+        ["piercing-rounds-magazine"] = 20,
+        ["construction-robot"] = 10,
+        ["modular-armor"] = 1,
+        ["solar-panel-equipment"] = 16,
+        ["belt-immunity-equipment"] = 1,
+        ["battery-equipment"] = 2,
+        ["personal-roboport-equipment"] = 1
         --[[
-        ['construction-robot'] = 50,
-        ['spidertron'] = 1,
-        ['spidertron-remote'] = 1,
-        ['modular-armor'] = 1,
-        ['solar-panel-equipment'] = 60,
-        ['belt-immunity-equipment'] = 1,
-        ['battery-equipment'] = 6,
-        ['personal-roboport-equipment'] = 5
+        ["construction-robot"] = 50,
+        ["spidertron"] = 1,
+        ["spidertron-remote"] = 1,
+        ["modular-armor"] = 1,
+        ["solar-panel-equipment"] = 60,
+        ["belt-immunity-equipment"] = 1,
+        ["battery-equipment"] = 6,
+        ["personal-roboport-equipment"] = 5
         ]]
     }
 }
